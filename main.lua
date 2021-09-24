bf678c6a3869a01b1a10d713e062aeb977999866
ANJDY <orangebookair@gmail.com>
2021-09-24 12:16:05 -0500

    Made changes

--- main.lua
+++ main.lua
@@ -1,4 +1,18 @@
+local str
+love = require('love')
+
+
+--Handles the initialization stuff like initial file stream readin 
+function love.load() 
+    str = "H"
+end
+
+
+--Called every tick
 function love.draw()
-    love.graphics.print("Hello World!", 100, 100)
+    
+    love.graphics.print(str, 0, 0)
+    str = str .. "E"
+
 end
 
