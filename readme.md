955a41ac6fa625619f5939484d9e9f2a48e45362
ANJDY <orangebookair@gmail.com>
2021-09-30 12:07:35 -0500

    Made m0re changes lmao

--- readme.md
+++ readme.md
@@ -1,2 +1,12 @@
 Run this file from main.lua with Alt+L 
     -> Extension: Love Launcher Handles That. 
+
+Love2d General docs:
+    love.graphics.draw(
+        image,
+        x,
+        y,
+        r,
+        sx,
+        sy
+    )
--- src/main.lua
+++ src/main.lua
@@ -4,37 +4,18 @@ require("QOL")
 --Read readme.md before doing anything
 local str
 local i
-local amongus
+
 
 --Handles the initialization stuff like initial file stream readin 
 function love.load() 
     str = "F"
     i = 0
-    amongus = love.graphics.newImage("among-us-logo.png")
+    imageTest = love.graphics.newImage("images/test.png")
 end
 
 
 --Called every tick, automatic heap clearing
 function love.draw()
-    coord1 =  {x = 1}
-    setmetatable (coord1, coordinateTemplate)
-    coord1.__index = coordinateTemplate
-
-    
-
-    
-    i = i + 0.0001
-    local i_temp = (1+i)*(1000*i)
-    --love.graphics.circle("fill", 10, 10, 1+i)
-    love.graphics.draw(
-        amongus,
-        0-i_temp, --X
-        0, --Y
-        0,  --Rotation
-        0.1 + i ,  --Scale X
-        0.2 -i        --Scale Y
-    )
-
-    love.graphics.print("X_Coord:" .. 0 - i_temp , 0, 0)
+    love.graphics.draw(imageTest)
 end
 
