123698ff16ca455e22d315fb6182518008301dd4
ANJDY <orangebookair@gmail.com>
2021-09-30 12:37:36 -0500

    change

--- src/main.lua
+++ src/main.lua
@@ -1,10 +1,14 @@
 require("templates")
 require("QOL")
+require("objects")
 
 --Read readme.md before doing anything
 local str = "F"
 local i = 0
 local sigmaTime = 0 
+local text = ""
+--
+
 
 
 --Handles the initialization stuff; mostly file stream reading
@@ -17,11 +21,36 @@ function love.update(deltaTime)
 
 end
 
---Called every tick, automatic heap clearing
+function love.textinput(t)
+    text = text .. t
+end
+
+--Called every tick, automatic heap and canvas clearing
 function love.draw()
-    if love.keyboard.isDown("down") then   -- reduce the value
-        
-        love.graphics.draw(imageTest)
+
+
+    -- Player WASD and Arrow Movement
+    if love.keyboard.isDown("down") or love.keyboard.isDown("s") then   -- reduce the value
+        player.y = player.y + player.speed
+    end 
+    if love.keyboard.isDown("up") or love.keyboard.isDown("w") then
+        player.y = player.y - player.speed
     end
+    if love.keyboard.isDown("left") or love.keyboard.isDown("a")then
+        player.x = player.x - player.speed
+    end
+    if love.keyboard.isDown("right") or love.keyboard.isDown("d")then
+        player.x = player.x + player.speed
+    end
+    --
+    love.graphics.draw(
+        imageTest,
+        player.x,
+        player.y,
+        player.r,
+        player.sx,
+        player.sy)
+    --
+    love.graphics.printf(text, 0, 0, love.graphics.getWidth())
 end
 
--- src/templates.lua
+++ src/templates.lua
@@ -1,6 +1,15 @@
+require("QOL")
+--
+
 coordinateTemplate = {}
 coordinateTemplate.y = 0
 coordinateTemplate.x = 0
-coordinateTemplate.__index = coordinateTemplate  -- Because Lua is dumb that's why
+setSelf(coordinateTemplate)  -- Because Lua is dumb that's why
 
 imageTemplate = {}
+
+objectTemplate = {}
+setmetatable(objectTemplate,coordinateTemplate)
+objectTemplate.sy = 1
+objectTemplate.sx = 1
+setSelf(objectTemplate)
